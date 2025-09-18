45a3c2289c16f2a74b57037d0d9a746d95b1f3e7
Atif-Bashir-1998 <adobe8832289@gmail.com>
2025-09-18 18:33:57 +0300

    flag resource created

---
+++ app/Filament/Resources/FlagResource/Pages/EditFlag.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Resources\FlagResource\Pages;
+
+use App\Filament\Resources\FlagResource;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditFlag extends EditRecord
+{
+    protected static string $resource = FlagResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\DeleteAction::make(),
+        ];
+    }
+}
