21e11f6860e663e0e00d73be4fa7f13cfb2577c0
Atif-Bashir-1998 <adobe8832289@gmail.com>
2025-09-18 13:49:55 +0300

    fixed styling issues on login and register pages

--- resources/js/pages/auth/Register.vue
+++ resources/js/pages/auth/Register.vue
@@ -102,104 +102,104 @@ const submit = () => {
 
 <template>
   <AuthLayout>
-    <v-card class="pa-4 pa-md-8" rounded="xl" elevation="4">
-    <v-card-title class="text-h5 font-weight-bold mb-2 text-center">
-      Create your account
-    </v-card-title>
-    <v-card-subtitle class="text-medium-emphasis mb-4 text-center">
-      Sign up to get started
-    </v-card-subtitle>
+    <v-card class="pa-4 pa-md-8 w-75" rounded="xl" elevation="4">
+      <v-card-title class="text-h5 font-weight-bold mb-2 text-center">
+        Create your account
+      </v-card-title>
+      <v-card-subtitle class="text-medium-emphasis mb-4 text-center">
+        Sign up to get started
+      </v-card-subtitle>
 
-    <v-form @submit.prevent="submit">
-      <div class="mb-4">
-        <v-text-field
-          id="name"
-          v-model="form.name"
-          label="Name"
-          type="text"
-          placeholder="Full name"
-          :error-messages="form.errors?.name"
-          variant="outlined"
-          rounded="lg"
-          required
-          autofocus
-        ></v-text-field>
-      </div>
+      <v-form @submit.prevent="submit">
+        <div class="mb-4">
+          <v-text-field
+            id="name"
+            v-model="form.name"
+            label="Name"
+            type="text"
+            placeholder="Full name"
+            :error-messages="form.errors?.name"
+            variant="outlined"
+            rounded="lg"
+            required
+            autofocus
+          ></v-text-field>
+        </div>
 
-      <div class="mb-4">
-        <v-text-field
-          id="email"
-          v-model="form.email"
-          label="Email address"
-          type="email"
-          placeholder="email@example.com"
-          :error-messages="form.errors?.email"
-          variant="outlined"
-          rounded="lg"
-          required
-        ></v-text-field>
-      </div>
+        <div class="mb-4">
+          <v-text-field
+            id="email"
+            v-model="form.email"
+            label="Email address"
+            type="email"
+            placeholder="email@example.com"
+            :error-messages="form.errors?.email"
+            variant="outlined"
+            rounded="lg"
+            required
+          ></v-text-field>
+        </div>
 
-      <div class="mb-4">
-        <v-text-field
-          id="password"
-          v-model="form.password"
-          label="Password"
-          type="password"
-          placeholder="Password"
-          :error-messages="form.errors?.password"
-          variant="outlined"
-          rounded="lg"
-          required
-        ></v-text-field>
-      </div>
+        <div class="mb-4">
+          <v-text-field
+            id="password"
+            v-model="form.password"
+            label="Password"
+            type="password"
+            placeholder="Password"
+            :error-messages="form.errors?.password"
+            variant="outlined"
+            rounded="lg"
+            required
+          ></v-text-field>
+        </div>
 
-      <div class="mb-6">
-        <v-text-field
-          id="password_confirmation"
-          v-model="form.password_confirmation"
-          label="Confirm password"
-          type="password"
-          placeholder="Confirm password"
-          :error-messages="form.errors?.password_confirmation"
-          variant="outlined"
-          rounded="lg"
-          required
-        ></v-text-field>
-      </div>
+        <div class="mb-6">
+          <v-text-field
+            id="password_confirmation"
+            v-model="form.password_confirmation"
+            label="Confirm password"
+            type="password"
+            placeholder="Confirm password"
+            :error-messages="form.errors?.password_confirmation"
+            variant="outlined"
+            rounded="lg"
+            required
+          ></v-text-field>
+        </div>
 
-      <v-btn
-        color="primary"
-        size="large"
-        rounded="lg"
-        block
-        type="submit"
-        class="mb-4"
-        :disabled="form.processing"
-      >
-        <v-progress-circular
-          v-if="form.processing"
-          size="20"
-          width="2"
-          color="white"
-          indeterminate
-          class="mr-2"
-        ></v-progress-circular>
-        Create account
-      </v-btn>
-
-      <div class="text-center text-body-2 text-medium-emphasis">
-        Already have an account?
         <v-btn
-          variant="text"
-          class="pa-0 text-blue-grey-darken-2 font-weight-bold"
-          :href="route('login')"
-          tabindex="-1"
+          color="primary"
+          size="large"
+          rounded="lg"
+          block
+          type="submit"
+          class="mb-4"
+          :disabled="form.processing"
         >
-          Log in
+          <v-progress-circular
+            v-if="form.processing"
+            size="20"
+            width="2"
+            color="white"
+            indeterminate
+            class="mr-2"
+          ></v-progress-circular>
+          Create account
         </v-btn>
-      </div>
-    </v-form>
-  </v-card>
+
+        <div class="text-center text-body-2 text-medium-emphasis">
+          Already have an account?
+          <v-btn
+            variant="text"
+            class="pa-0 text-blue-grey-darken-2 font-weight-bold"
+            :href="route('login')"
+            tabindex="-1"
+          >
+            Log in
+          </v-btn>
+        </div>
+      </v-form>
+    </v-card>
   </AuthLayout>
 </template>
