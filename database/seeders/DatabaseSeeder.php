9b71cb4a2accc1cb963df087103b48fcaa9dd730
Atif-Bashir-1998 <adobe8832289@gmail.com>
2025-09-18 13:59:34 +0300

    predefined roles of the app are created

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,10 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Utils\RoleHelper;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +15,28 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        Role::create(['name' => RoleHelper::DEFAULT_ROLES['ADMIN']]);
+        Role::create(['name' => RoleHelper::DEFAULT_ROLES['DEVELOPER']]);
+        Role::create(['name' => RoleHelper::DEFAULT_ROLES['MODERATOR']]);
+
+        $admin = User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+        ]);
+        $admin->assignRole(RoleHelper::DEFAULT_ROLES['ADMIN']);
+
+        $developer = User::factory()->create([
+            'name' => 'Developer',
+            'email' => 'developer@example.com',
+        ]);
+        $developer->assignRole(RoleHelper::DEFAULT_ROLES['DEVELOPER']);
+
+        $moderator = User::factory()->create([
+            'name' => 'Moderator',
+            'email' => 'moderator@example.com',
+        ]);
+        $moderator->assignRole(RoleHelper::DEFAULT_ROLES['MODERATOR']);
+
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
