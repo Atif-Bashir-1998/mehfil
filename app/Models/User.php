14d08992ca4e33746f353f05076ea9eaa763d052
Atif-Bashir-1998 <adobe8832289@gmail.com>
2025-09-18 13:52:38 +0300

    added spatie/laravel-permission to the project

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,11 +10,12 @@ use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, Hasroles;
 
     /**
      * The attributes that are mass assignable.
